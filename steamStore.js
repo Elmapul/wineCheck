b5aef9d89cbf9804acb4daebef48e97b48697b43
Elmapul <igccpao@gmail.com>
2017-06-20 03:19:52 -0300

    neptunia and tombraider series added

--- steamStore.js
+++ steamStore.js
@@ -2,8 +2,30 @@
 var WG=[]; //(wine-able games)
 WG[282900]="gold";	//neptunia rebirth 1
 WG[351710]="gold";	//neptunia rebirth 2
-WG[353270]="gold";	//neptunia rebirth 1
-WG[225300]="silver";	//tombraider 2
+WG[353270]="gold";	//neptunia rebirth 3
+
+WG[496810]="untested";//MegaTagmension Blanc + Neptune VS Zombies (Neptunia)
+WG[387340]="bronze";	//Hyperdimension Neptunia U: Action Unleashed
+WG[460120]="bronze";   //Megadimension Neptunia VII
+WG[415480]="garbage";	 //Hyperdevotion Noire: Goddess Black Heart (Neptunia)
+WG[571530]="untested"; //Superdimension Neptune VS Sega Hard Girls
+
+WG[224960]="";//Tomb Raider I
+WG[225300]="silver";	//tombraider II
+WG[225320]="platinum";	//Tomb Raider III: Adventures of Lara Croft
+WG[224980]="bronze";//Tomb Raider IV: The Last Revelation //GOG
+WG[225000]="platinum";//Tomb Raider V: Chronicles //first relase
+WG[225020]="gold";//Tomb Raider VI: The Angel of Darkness
+WG[8140]="platinum";//Tomb Raider: Underworld //STEAM -bronze
+WG[7000]="platinum"//Tomb Raider legend 
+WG[289690]="";//LARA CROFT AND THE TEMPLE OF OSIRIS™
+WG[35130]="platinum";//Lara Croft and the Guardian of Light
+//203160 Tomb Raider (2013) nativo //platinum
+WG[391220]="bronze";//Rise of Tomb Raider
+
+
+WG[]="";//
+
 
 var aux1= document.getElementsByClassName("search_result_row");
 aux3=document.getElementsByClassName("responsive_search_name_combined");
